3a274c0876ba6fb78de385b7fb0d59c5a62ffcd1
hanhyur <zeidgame@gmail.com>
2024-07-22 14:28:04 +0900

    chore : 프로젝트 생성
    <description>
    >> 좋아요 기능 프로젝트
    
    <issue>

---
+++ liked/src/main/java/me/hanhyur/liked/LikedApplication.java
@@ -0,0 +1,13 @@
+package me.hanhyur.liked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikedApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(LikedApplication.class, args);
+    }
+    
+}
---
+++ liked/src/test/java/me/hanhyur/liked/LikedApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hanhyur.liked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikedApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
