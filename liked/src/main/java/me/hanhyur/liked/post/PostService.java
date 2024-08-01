0f3f761dd05e5a17c5015aef79a56ecb48669ce2
hanhyur <zeidgame@gmail.com>
2024-08-01 13:58:37 +0900

    feat : API Test
    <description>
    >> api 테스트로 전환
    
    <issues>

--- liked/src/main/java/me/hanhyur/liked/post/PostService.java
+++ liked/src/main/java/me/hanhyur/liked/post/PostService.java
@@ -1,18 +1,27 @@
 package me.hanhyur.liked.post;
 
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/posts")
 class PostService {
     private final PostPort postPort;
     
-    PostService(PostPort postPort) {
+    PostService(final PostPort postPort) {
         this.postPort = postPort;
     }
     
-    public void addPost(AddPostRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> addPost(@RequestBody final AddPostRequest request) {
         final Post post = new Post(request.content(), request.imageUrl(), request.location());
         
         postPort.save(post);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
---
+++ liked/src/test/java/me/hanhyur/liked/post/PostApiTest.java
@@ -0,0 +1,40 @@
+package me.hanhyur.liked.post;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import me.hanhyur.liked.ApiTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class PostApiTest extends ApiTest {
+
+    @Test
+    void createPost() {
+        final var request = createPostEntity();
+        
+        final var response = createPostRequest(request);
+        
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+    
+    private static ExtractableResponse<Response> createPostRequest(final AddPostRequest request) {
+        return RestAssured.given().log().all()
+          .contentType("application/json")
+          .body(request)
+          .when()
+          .post("/posts")
+          .then()
+          .log().all().extract();
+    }
+    
+    private static AddPostRequest createPostEntity() {
+        final String content = "this is fake content";
+        final String imageUrl = "Roughly speaking, the image URL";
+        final String location = "Seoul, Republic of Korea";
+        
+        return new AddPostRequest(content, imageUrl, location);
+    }
+}
